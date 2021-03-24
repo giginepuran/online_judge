a0eb73ab0dcff8a716c95c9cb06f7ddbf61b7426
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-03-24 18:18:36 +0800

    Create 3_LengthOfLongestSubstring.cs

---
+++ leetcode/3_LengthOfLongestSubstring.cs
@@ -0,0 +1,30 @@
+// Medium https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int max_l = 0, L = s.Length;
+        int[] same_position = new int[L];
+        for(int i = 0; i < L; i++){
+            same_position[i] = -1;
+            for(int j = i-1; j >= 0; j--){
+                if(s[j] == s[i]){
+                    same_position[i] = j;
+                    break;
+                }
+            }
+        }
+        for(int i = 0; (i < L) && (i+max_l)<L; i++){
+            int j = i+1, ll = 0;
+            while(j < L){
+                if(same_position[j] >= i) {
+                    ll = j-i;
+                    i = same_position[j];
+                    break;
+                }
+                j++;
+            }
+            ll = (j==L)? (j-i):ll;
+            max_l = (max_l<ll)? ll:max_l;
+        }
+        return max_l;
+    }
+}
