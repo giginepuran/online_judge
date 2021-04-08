2cd78d578da168d7b831c09a6afd59b4fa6c6020
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-04-08 20:27:28 +0800

    Create 20_Valid_Parentheses.cs

---
+++ leetcode/20_Valid_Parentheses.cs
@@ -0,0 +1,54 @@
+/*
+Day14 (4/8)
+Runtime PR: 94.38
+Memory PR: 81.95
+List is fast!
+*/
+public class Solution {
+    public bool IsValid(string s) {
+        List<char> bras = new List<char>(){};
+        int layer = -1;
+        for(int i = 0; i < s.Length; i++){
+            if(IsBra(s[i])){
+                layer++;
+                bras.Add(s[i]);
+            }
+            else{
+                if(layer<0) return false;
+                if(bras[bras.Count-1]==ToSet(s[i])){
+                    layer--;
+                    bras.RemoveAt(bras.Count-1);
+                }
+                else return false;
+            }
+        }
+        return (layer==-1)?true:false;
+        char ToSet(char c){
+            switch(c){
+                case ')':
+                return '(';
+                case '}':
+                return '{';
+                default :
+                return '[';
+            }
+        }
+
+        bool IsBra(char c) {
+            switch(c){
+                case '(':
+                return true;
+                case '{':
+                return true;
+                case '[':
+                return true;
+                case ')':
+                return false;
+                case '}':
+                return false;
+                default:
+                return false;
+            }
+        }
+    }
+}
