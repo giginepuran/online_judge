80a388f48d2c4037831713fa0fa0ca5942a26aa0
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-04-10 16:24:24 +0800

    Update 1_Two_Sum.cs

--- leetcode/1_Two_Sum.cs
+++ leetcode/1_Two_Sum.cs
@@ -13,3 +13,42 @@ public class Solution {
         return new int[]{0, 0};
     }
 }
+/*
+4/10 update
+Runtime: 
+220ms
+PR 99.81
+Memory: 
+32MB
+PR 15.69
+*/
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var nums_copy = (int[]) nums.Clone();
+        Array.Sort(nums_copy);
+        for(int i = 0, j = nums.Length-1; i<j;){
+            int sum = nums_copy[i] + nums_copy[j];
+            if (sum > target) j--;
+            else if (sum < target) i++;
+            else{
+                for (int ii = 0; ii < nums.Length; ii++) {
+                    if (nums[ii] == nums_copy[i]){
+                        for (int jj = ii+1; jj < nums.Length; jj++){
+                            if (nums[jj] == nums_copy[j]){
+                                return new int[]{ii, jj};
+                            }
+                        }
+                    }
+                    else if (nums[ii] == nums_copy[j]){
+                        for (int jj = ii+1; jj < nums.Length; jj++){
+                            if (nums[jj] == nums_copy[i]){
+                                return new int[]{ii, jj};
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
