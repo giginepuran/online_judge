69db1fd42470f54f5c39bf0aa35fc8f4aa9c739a
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-12-04 23:11:15 +0900

    Create 28.cs

---
+++ leetcode/28.cs
@@ -0,0 +1,32 @@
+/** 2021.12.04
+ * Runtime PR 74.82
+ * Memory PR 50.70
+ */
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length == 0) return 0;
+        int dup = -1;
+        foreach (char c in needle) {
+            if (c == needle[0])
+                dup += 1;
+            else 
+                break;
+        }
+        int i, shift;
+        bool found;
+        for(i = 0; i < haystack.Length && needle.Length <= haystack.Length-i; i++) {
+            found = true;
+            shift = 0;
+            foreach (char c in needle) {
+                if (haystack[i+shift] != c) {
+                    found = false;
+                    break;
+                }
+                shift++;
+            }
+            if (found) return i;
+        }
+        return -1;
+    }
+}
