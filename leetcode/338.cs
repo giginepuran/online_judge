2496971af7542b670d0a5dee68907c98beec0ea7
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-03-01 16:59:30 +0800

    Update 338.cs

--- leetcode/338.cs
+++ leetcode/338.cs
@@ -19,3 +19,48 @@ public class Solution {
         return ans;
     }
 }
+
+/*
+做法是類似費波納氣數列的做法，從小做到大，大的會回去取小的結果
+0 1 
+0 1 ( 2%2 + ans(2/2) ) ( 3%2 + ans(3/2) ) ...
+
+Time complexity = O(n)
+------------------------------------------------
+本來想要用類似slide window的作法，但不知道為何速度沒辦法超越單純迴圈= =
+0 1
+
+0 1 (0+1) (1+1)
+0 1 1 2
+
+0 1 1 2 (0+1) (1+1) (1+1) (2+1)
+0 1 1 2 1 2 2 3
+
+Time complexity = O(logn) ?吧 看起來是= =
+有考量到可能是array與Ienum間轉換的時間所導致，
+而一開始就把[0, 1]宣告成IEnum還是快不起來
+
+...
+*/
+
+/* Sliding window的作法
+Runtime 67.82% 
+Memory   5.25% 
+*/
+public class Solution {
+    public int[] CountBits(int n) 
+    {
+        var series = Enumerable.Range(0, 2);
+
+        int length = 2;
+        while (length <= n)
+        {
+            series = series.Concat(
+                     from num in series
+                     select num+1);
+            length *= 2;
+        }
+        series = series.Take(n+1);
+        return series.ToArray();
+    }
+}
