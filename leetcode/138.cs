56dfe7bbad6c031c59d2f932655c6a15a365c08b
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-03-12 20:11:15 +0800

    Update 138.cs

--- leetcode/138.cs
+++ leetcode/138.cs
@@ -1,5 +1,44 @@
 /* Medium
 03/12/22
-Runtime -%
-Memory  -%
+19:42 start
+
+AC_1 8:10 (28 min)
+Runtime 76.82%
+Memory  43.26%
 */
+
+public class Solution {
+    public Node CopyRandomList(Node head) {
+        if (head == null) return null;
+
+        // clone nodes with same val only
+        Node newHead = new Node(head.val);
+        Node operating = head.next;
+        Node operating2 = newHead;
+        while (operating != null)
+        {
+            operating2.next = new Node(operating.val);
+            operating2 = operating2.next;
+            operating = operating.next;
+        }
+
+        // sync two nodes' pointer
+        operating = head;
+        operating2 = newHead;
+        while (operating != null)
+        {
+            Node surve = head;
+            Node surve2 = newHead;
+            while (surve != operating.random)
+            {
+                surve = surve.next;
+                surve2 = surve2.next;
+            }
+            operating2.random = surve2;
+            operating = operating.next;
+            operating2 = operating2.next;
+        }
+
+        return newHead;   
+    } 
+}
