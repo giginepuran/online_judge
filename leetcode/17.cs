85150dcfba2eb935e2b1db882d848c03f1d87223
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-03-14 19:04:52 +0800

    Update and rename 17_Letter_Combinations_of_a_Phone_Number.cs to 17.cs

--- leetcode/17.cs
+++ leetcode/17.cs
@@ -2,15 +2,6 @@
 Day11 (4/4)
 Runtime PR: 97.80
 Memory PR: 63.34
-Points
-1. Write recursion into class
-a. Using pointer(ref) without static recursion
-b. Save memory with table / digits / layer...
-2. Learned difference between out & ref
-3. Learned the tree structure in coding!!!!
-a. pointer is important to tree
-b. combine pointer & recursion , 
-pass your table/object can make the code faster and less RAM-usage!
 */
 
 public class Solution {
@@ -31,6 +22,6 @@ public class Solution {
                     add2List(ref combinations, str+c, layer+1);
                 }
             }
-        } // End of add2List
-    } // End of Letter Combinations
-} // End of Solution
+        }
+    }
+}
