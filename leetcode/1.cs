18e5165270c1a880f0f64fdcd67ad4b51736efcb
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-03-14 19:06:12 +0800

    Update and rename 1_Two_Sum.cs to 1.cs

--- leetcode/1.cs
+++ leetcode/1.cs
@@ -1,25 +1,8 @@
-// Easy
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        int l = nums.Length;
-        for(int i = 0; i < l; i++){
-            for(int j = l-1; j > i; j--){
-                if((nums[i] + nums[j])==target){
-                    int[] result = new int[2] {i, j};
-                    return result;
-                }
-            }
-        }
-        return new int[]{0, 0};
-    }
-}
 /*
-4/10 update
+4/10 
 Runtime: 
-220ms
 PR 99.81
 Memory: 
-32MB
 PR 15.69
 */
 public class Solution {
