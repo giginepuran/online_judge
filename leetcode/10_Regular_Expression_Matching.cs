e5215b46443a469200f1c811645ec70a3b2dac76
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-03-14 18:22:06 +0800

    Update 10_Regular_Expression_Matching.cs

--- leetcode/10_Regular_Expression_Matching.cs
+++ leetcode/10_Regular_Expression_Matching.cs
@@ -1,7 +1,3 @@
-// Recursion is used
-// runtime PR is only 76.53
-// Maybe dynamic programming can make it faster?
-// But I have no idea how to use dynamic programming on it.
 public class Solution {
     public bool IsMatch(string s, string p) {
         return match(s, p, s.Length-1, p.Length-1);
@@ -9,7 +5,6 @@ public class Solution {
     public bool match(string s, string p, int i, int j) {
         if(i==-1 && j==-1) return true;
         if(j==-1) return false;
-        // if(i==-1) return (p[j]!='*')? false:match(s, p, i, j-1);
         if(p[j]=='*') {
             if (i==-1) return match(s, p, i, j-2);
             if (s[i]==p[j-1] || p[j-1]=='.'){
@@ -23,4 +18,4 @@ public class Solution {
         if(s[i]==p[j] || p[j]=='.') return match(s, p, i-1, j-1);
         return false;
     }
-} // End of Solution
+}
