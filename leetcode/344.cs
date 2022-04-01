0b87b4ce6dea3165381a9f962135a884a9e581b1
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-04-01 09:57:11 +0800

    Update 344.cs

--- leetcode/344.cs
+++ leetcode/344.cs
@@ -1,4 +1,19 @@
 /* easy
 09:50 start
-00:00 AC1
+09:56 AC1
 */
+
+public class Solution {
+    public void ReverseString(char[] s) {
+        char buffer = ' ';
+        for (int i = 0; i < s.Length; ++i)
+        {
+            for (int j = 0; j < s.Length - i - 1; ++j)
+            {
+                buffer = s[j];
+                s[j] = s[j+1];
+                s[j+1] = buffer;
+            }
+        }
+    }
+}
