0afb90d1e0811033baefb547e4b44c350409f3dc
giginepuran <61973060+giginepuran@users.noreply.github.com>
2022-04-01 18:19:28 +0800

    Update 32.cs

--- leetcode/32.cs
+++ leetcode/32.cs
@@ -1,6 +1,48 @@
 /* Hard
 17:30 start
-00:00 AC1
-Runtime: 00.00%
-Memory:  00.00%
+18:14 AC1
+Runtime: 99.53%
+Memory:   5.67%
+
+最佳解是用抓bra數量=key數量，的方式來求最長，
+沒有用任何Dictionary/Stack有點厲害的想法@@
+
+利用Dictionary + Stack 以 O(n)整理過整個braket
+本來最後算長度的部分好像無法再最佳化了
+因為KeyValuePair是無序的，只能用foreach@@
 */
+
+public class Solution {
+    public int LongestValidParentheses(string s) {
+        Dictionary<int,int> pair = new Dictionary<int, int>();
+        Stack<int> bra = new Stack<int>();
+        Stack<int> abandKet = new Stack<int>();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (s[i] == '(')
+                bra.Push(i);
+            else
+            {
+                if (bra.Count != 0)
+                    pair.Add(bra.Pop(), i);
+                else
+                    abandKet.Push(i);
+            }
+        }
+        if (pair.Count == 0)
+            return 0;
+        int ans = 0;
+        foreach (var kv in pair)
+        {
+            int head = kv.Key;
+            int tail = kv.Value;
+            while (pair.ContainsKey(tail+1))
+            {
+                tail = pair[tail+1];
+            }
+            if (ans < tail - head + 1)
+                ans = tail - head + 1;
+        }
+        return ans;
+    }
+}
