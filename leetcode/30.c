0ec407915aec11fd731405ee69eceefb6cad05d9
giginepuran <61973060+giginepuran@users.noreply.github.com>
2024-11-28 23:06:37 +0900

    Update 30.c

--- leetcode/30.c
+++ leetcode/30.c
@@ -87,25 +87,156 @@ Dictionary *compare_result = NULL;
 // ------------------------------^^^definition of Dict^^^---------------------------
 
 typedef struct Node {
+    int set_index;
     char *word;
     int *word_len;
-    struct Node *next;
+    Node *next;
+    Node *next2;
 } Node;
 
-int isMatchNode(char *s, Node *node) {
-    int ret;
-    if (strncmp(s, word, strlen(word)))
+int *ans;
+int ansSize;
+int sLen;
+Node *holdNodes;
+Node *setNodes;
+
+Node *createNode(char *word) {
+    Node *p = (Node*)malloc(sizeof(Node));
+    p->next = NULL;
+    p->next2 = NULL;
+    p->word = word;
+    p->word_len = strlen(word);
+    p->set_index = -1;
+    return p;
 }
 
+void freeNodes(Node *node) {
+    Node *p;
+    while (node) {
+        p = node->next;
+        free(node);
+        node = p;
+    }
+    return;
+}
 
-int *ans;
-int sLen;
+void enNode(Node *head, Node *node) {
+    Node *p;
+    if (!head) {
+        head = node;
+        return;
+    }
+    p = head;
+    while (p->next)
+        p = p->next;
+    p->next = node;
+    return;
+}
+
+Node *deNode(Node *head, Node *node) {
+    Node *p;
+    if (!head)
+        return NULL;
+    if (head == node) {
+        head = NULL;
+        p = node->next;
+        node->next = NULL;
+        return p;
+    }
+    p = head;
+    while (p->next) {
+        if (p->next == node) {
+            p->next = NULL;
+            p = node->next;
+            node->next = NULL;
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
+}
+
+void nextCopyToNext2(Node *head) {
+    Node *p;
+    if (!head)
+        return;
+    p = head;
+    while (p) {
+        p->next2 = p->next;
+        p = p->next2;
+    }
+    return;
+}
+
+int isMatchNode(char *s, int i, Node *node) {
+    int result;
+    int *values;
+    int j;
+
+    if (strncmp(&s[i], node->word, node->word_len)) {
+        result = COMPARE_RESULT_FAIL;
+        
+    } else {
+        result = COMPARE_RESULT_SUCCESS;
+    }
+
+    values = lookup(compare_result, node->word);
+    if (!values) {
+        values = (int*)malloc(sizeof(int) * 10000);
+        for (j = 0; j < 10000; j++) {
+            values[j] = COMPARE_RESULT_NOTYET;
+        }
+        insert(compare_result, node->word, values);
+        free(values);
+        values = lookup(compare_result, node->word);
+    }
+    values[i] = result;
+    return result;
+}
+
+int dfs(char *s, int i) {
+    Node *p;
+    Node *matchNodes;
+    Node *remainNodes;
+
+    if (!holdNodes) {
+        ans[ansSize++] = setNodes->set_index;
+        return 1;
+    }
+
+    p = holdNodes;
+    while (p) {
+        if (isMatchNode(s, i, p) == COMPARE_RESULT_SUCCESS) {
+            remainNodes = deNode(holdNodes, p);
+            enNode(matchNodes, p);
+            p = remainNodes;
+            enNode(holdNodes, p);
+        } else {
+            p = p->next;
+        }
+    }
+    nextCopyToNext2(matchNodes);
+
+    p = matchNodes;
+    while (p) {
+        Node *p2;
+        
+        remainNodes = deNode(matchNodes, p);
+        p->set_index = i;
+        enNode(setNodes, p);
+        enNode(holdNodes, remainNodes);
+        if (dfs(s, i + p->word_len))
+            return 1;
+        p = p->next2;
+    }
+
+    return 0;
+}
 
 int* findSubstring(char* s, char** words, int wordsSize, int* returnSize) {
     int i = 0, ansSize = 0;
     ans = (int*)malloc(sizeof(int) * 10000);
 
-    
     *returnSize = ansSize;
     return ans;
 }
@@ -124,5 +255,3 @@ int main() {
     }
     return 0;
 }
-
-
